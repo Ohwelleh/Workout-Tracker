08e02c7256b561e5978dab074a54a4f17e6c955e
Ohwelleh <austin7955@hotmail.com>
2022-04-30 15:45:57 -0700

    Recreated the project in IntelliJ

---
+++ workout/src/com/workout/main/WorkoutApplication.java
@@ -0,0 +1,17 @@
+package com.workout.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WorkoutApplication extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
