a5bb55b293a16f65f3adc52c4de3d811fa914497
Ohwelleh <austin7955@hotmail.com>
2022-05-08 11:15:03 -0700

    Updated Maven Javafx to version 18.0.1, Added scenes for the Big 3 graphs and Personal Bests.

--- workouttracker/src/main/java/com/workouttracker/main/Controller/BigThreeController.java
+++ workouttracker/src/main/java/com/workouttracker/main/Controller/BigThreeController.java
@@ -1,55 +1,45 @@
 package com.workouttracker.main.Controller;
 
 import com.workouttracker.main.WorkoutApplication;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
-import com.workouttracker.main.WorkoutApplication.*;
 
-public class WorkoutController {
-
+public class BigThreeController {
     @FXML
     private Button BigThreeBtn;
     @FXML
     private HBox BigWindow;
     @FXML
-    private Button WorkoutsBtn;
-    @FXML
     private Label MainLabel;
     @FXML
-    private HBox DateCard;
+    private Button WorkoutsBtn;
     @FXML
-    private TableView EntryTable;
-    @FXML
-    private Button PeronalBestBtn;
+    private Button PersonalBestBtn;
 
     @FXML
     public void LoadBigThree(Event event) {
-        try {
-            WorkoutApplication.changeScene("bigthree-view.fxml");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-    @FXML
-    public void LoadWorkouts(Event event) {
-        DateCard.setVisible(true);
-        MainLabel.setText("Workouts");
-        EntryTable.setVisible(true);
     }
 
     @FXML
-    public void loadPB(Event event) {
+    public void LoadWorkouts(Event event) {
+        try {
+            WorkoutApplication.changeScene("workout-view.fxml");
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void loadPersonalBest(Event event) {
         try {
             WorkoutApplication.changeScene("personalbest-view.fxml");
         }catch (Exception e){
 
             e.printStackTrace();
         }
-
     }
 }
--- workouttracker/src/main/java/com/workouttracker/main/WorkoutApplication.java
+++ workouttracker/src/main/java/com/workouttracker/main/WorkoutApplication.java
@@ -8,15 +8,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class WorkoutApplication extends Application {
+
+    private static Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
+        primaryStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(WorkoutApplication.class.getResource("workout-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1315, 810);
-        stage.setTitle("Workout Tracker");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Workout Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    public static void changeScene(String fxmlFile) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(WorkoutApplication.class.getResource(fxmlFile));
+        Scene scene = new Scene(fxmlLoader.load(), 1315, 810);
+        primaryStage.setScene(scene);
+
+    }
     public static void main(String[] args) {
         launch();
     }
