7dc1a370ba7b4d002e7b6d81751a7ff6b47043cd
Ohwelleh <austin7955@hotmail.com>
2022-05-09 14:26:30 -0700

    Implemented the line charts on the Big 3 section, using mock data.

--- workouttracker/src/main/java/com/workouttracker/main/Controller/BigThreeController.java
+++ workouttracker/src/main/java/com/workouttracker/main/Controller/BigThreeController.java
@@ -7,6 +7,10 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -27,6 +31,24 @@ public class BigThreeController {
     private Button WorkoutsBtn;
     @FXML
     private Button PersonalBestBtn;
+    @FXML
+    private LineChart<String, Number> liftGraph;
+
+    @FXML
+    private void initialize(){
+        liftGraph.setAnimated(false);
+        liftGraph.setTitle("Bench");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Strength Weights");
+        series.getData().add(new XYChart.Data<>("Apr 11", 150));
+        series.getData().add(new XYChart.Data<>("Apr 18", 165));
+        series.getData().add(new XYChart.Data<>("Apr 25", 165));
+        series.getData().add(new XYChart.Data<>("May 2", 170));
+        series.getData().add(new XYChart.Data<>("May 9", 175));
+
+        liftGraph.getData().add(series);
+    }
 
     @FXML
     public void LoadBigThree(Event event) {
@@ -64,14 +86,55 @@ public class BigThreeController {
 
     @FXML
     public void LoadBenchGraph(Event event) {
+
+        liftGraph.getData().clear();
+
+        liftGraph.setTitle("Bench");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
+        series.setName("Strength Weights");
+        series.getData().add(new XYChart.Data<>("Apr 11", 150));
+        series.getData().add(new XYChart.Data<>("Apr 18", 165));
+        series.getData().add(new XYChart.Data<>("Apr 25", 165));
+        series.getData().add(new XYChart.Data<>("May 2", 170));
+        series.getData().add(new XYChart.Data<>("May 9", 175));
+
+        liftGraph.getData().add(series);
+
     }
 
     @FXML
     public void LoadSquatGraph(Event event) {
+        liftGraph.getData().remove(0);
+
+        liftGraph.setTitle("Squat");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Strength Weights");
+        series.getData().add(new XYChart.Data<>("Apr 11", 250));
+        series.getData().add(new XYChart.Data<>("Apr 18", 265));
+        series.getData().add(new XYChart.Data<>("Apr 25", 165));
+        series.getData().add(new XYChart.Data<>("May 2", 270));
+        series.getData().add(new XYChart.Data<>("May 9", 175));
+
+        liftGraph.getData().add(series);
     }
 
     @FXML
     public void LoadDeadliftGraph(Event event) {
+        liftGraph.getData().remove(0);
+
+        liftGraph.setTitle("Deadlift");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Strength Weights");
+        series.getData().add(new XYChart.Data<>("Apr 11", 224));
+        series.getData().add(new XYChart.Data<>("Apr 18", 200));
+        series.getData().add(new XYChart.Data<>("Apr 25", 333));
+        series.getData().add(new XYChart.Data<>("May 2", 400));
+        series.getData().add(new XYChart.Data<>("May 9", 123));
+
+        liftGraph.getData().add(series);
     }
 
     public void getDataModel(BigThreeData bigData){
